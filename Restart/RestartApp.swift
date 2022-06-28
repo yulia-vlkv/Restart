0313abf0f84ecf39884e0bcce0663ba9a28631d0
Iuliia Vlkv <yu.wlkw@gmail.com>
2022-06-28 09:59:11 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Iuliia Volkova on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
